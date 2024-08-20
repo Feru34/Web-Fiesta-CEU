3d4b20a2588b058f299d067a69227b3b3658650a
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-08-19 19:41:04 -0500

    Agregar imagenes  sin parciales

--- src/components/BirthdayForm.js
+++ src/components/BirthdayForm.js
@@ -2,21 +2,84 @@ import React, { useState } from 'react';
 import '../styles/BirthdayForm.css';
 
 const zodiacSigns = [
-  { sign: 'Aries', start: '03-21', end: '04-19' },
-  { sign: 'Tauro', start: '04-20', end: '05-20' },
-  { sign: 'Géminis', start: '05-21', end: '06-20' },
-  { sign: 'Cáncer', start: '06-21', end: '07-22' },
-  { sign: 'Leo', start: '07-23', end: '08-22' },
-  { sign: 'Virgo', start: '08-23', end: '09-22' },
-  { sign: 'Libra', start: '09-23', end: '10-22' },
-  { sign: 'Escorpio', start: '10-23', end: '11-21' },
-  { sign: 'Sagitario', start: '11-22', end: '12-21' },
-  { sign: 'Capricornio', start: '12-22', end: '01-19' },
-  { sign: 'Acuario', start: '01-20', end: '02-18' },
-  { sign: 'Piscis', start: '02-19', end: '03-20' }
+  { 
+    sign: 'Acuario', 
+    start: '01-20', 
+    end: '02-18',
+    images: ['/images/1.png', '/images/2.png', '/images/3.png', '/images/4.png'],
+  },
+  { 
+    sign: 'Piscis', 
+    start: '02-19', 
+    end: '03-20',
+    images: ['/images/Piscis1.png', '/images/Piscis2.png', '/images/Piscis3.png', '/images/Piscis4.png']
+  },
+  { 
+    sign: 'Aries', 
+    start: '03-21', 
+    end: '04-19',
+    images: ['/images/Aries1.png', '/images/Aries2.png', '/images/Aries3.png', '/images/Aries4.png']
+  },
+  { 
+    sign: 'Tauro', 
+    start: '04-20', 
+    end: '05-20',
+    images: ['/images/Tauro1.png', '/images/Tauro2.png', '/images/Tauro3.png', '/images/Tauro4.png']
+  },
+  { 
+    sign: 'Géminis', 
+    start: '05-21', 
+    end: '06-20',
+    images: ['/images/Géminis1.png', '/images/Géminis2.png', '/images/Géminis3.png', '/images/Géminis4.png']
+  },
+  { 
+    sign: 'Cáncer', 
+    start: '06-21', 
+    end: '07-22',
+    images: ['/images/Cáncer1.png', '/images/Cáncer2.png', '/images/Cáncer3.png', '/images/Cáncer4.png']
+  },
+  { 
+    sign: 'Leo', 
+    start: '07-23', 
+    end: '08-22',
+    images: ['/images/Leo1.png', '/images/Leo2.png', '/images/Leo3.png', '/images/Leo4.png']
+  },
+  { 
+    sign: 'Virgo', 
+    start: '08-23', 
+    end: '09-22',
+    images: ['/images/Virgo1.png', '/images/Virgo2.png', '/images/Virgo3.png', '/images/Virgo4.png']
+  },
+  { 
+    sign: 'Libra', 
+    start: '09-23', 
+    end: '10-22',
+    images: ['/images/Libra1.png', '/images/Libra2.png', '/images/Libra3.png', '/images/Libra4.png']
+  },
+  { 
+    sign: 'Escorpio', 
+    start: '10-23', 
+    end: '11-21',
+    images: ['/images/Escorpio1.png', '/images/Escorpio2.png', '/images/Escorpio3.png', '/images/Escorpio4.png']
+  },
+  { 
+    sign: 'Sagitario', 
+    start: '11-22', 
+    end: '12-21',
+    images: ['/images/Sagitario1.png', '/images/Sagitario2.png', '/images/Sagitario3.png', '/images/Sagitario4.png']
+  },
+  { 
+    sign: 'Capricornio', 
+    start: '12-22', 
+    end: '01-19',
+    images: ['/images/Capricornio1.png', '/images/Capricornio2.png', '/images/Capricornio3.png', '/images/Capricornio4.png']
+  }
 ];
 
+
 const songs = {
+  Acuario: 'Canción para Acuario',
+  Piscis: 'Canción para Piscis',
   Aries: 'Canción para Aries',
   Tauro: 'Canción para Tauro',
   Géminis: 'Canción para Géminis',
@@ -26,9 +89,7 @@ const songs = {
   Libra: 'Canción para Libra',
   Escorpio: 'Canción para Escorpio',
   Sagitario: 'Canción para Sagitario',
-  Capricornio: 'Canción para Capricornio',
-  Acuario: 'Canción para Acuario',
-  Piscis: 'Canción para Piscis'
+  Capricornio: 'Canción para Capricornio'
 };
 
 const BirthdayForm = ({ setZodiacInfo }) => {
@@ -45,9 +106,11 @@ const BirthdayForm = ({ setZodiacInfo }) => {
     );
 
     if (zodiac) {
+      const randomImage = zodiac.images[Math.floor(Math.random() * zodiac.images.length)];
       setZodiacInfo({
         sign: zodiac.sign,
         song: songs[zodiac.sign],
+        image: randomImage
       });
     }
   };
@@ -56,7 +119,7 @@ const BirthdayForm = ({ setZodiacInfo }) => {
     <form className="birthday-form" onSubmit={handleSubmit}>
       <p>Danos tu signo y te diremos tu canción de la noche </p>
       <div className="date-inputs">
-      <input
+        <input
           type="number"
           placeholder="Día (DD)"
           value={day}
--- src/components/ZodiacResult.js
+++ src/components/ZodiacResult.js
@@ -4,8 +4,12 @@ import '../styles/ZodiacResult.css';
 const ZodiacResult = ({ zodiacInfo }) => {
   return (
     <div className="zodiac-result">
-      <h2>Tu signo es {zodiacInfo.sign}!</h2>
-      <p>Te recomendamos escuchar: {zodiacInfo.song}</p>
+      <h2>Tu signo es {zodiacInfo.sign}</h2>
+      <p>Esta será tu canción de la noche</p>
+      {/* Mostrar la imagen del signo */}
+      <div className="image-container">
+        <img src={zodiacInfo.image} alt={`Imagen de ${zodiacInfo.sign}`} className="zodiac-image" />
+      </div>
       <button onClick={() => window.location.reload()}>Probar otra vez</button>
     </div>
   );
