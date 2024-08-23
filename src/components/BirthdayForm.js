ba2cdce08fa1d274e48a3ce95c8ecce645706c58
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-08-23 10:21:36 -0500

    actualizaciones imagenes

--- src/components/BirthdayForm.js
+++ src/components/BirthdayForm.js
@@ -6,90 +6,91 @@ const zodiacSigns = [
     sign: 'Acuario', 
     start: '01-20', 
     end: '02-18',
-    images: ['/images/1.png', '/images/2.png', '/images/3.png', '/images/4.png'],
+    images: ['/images/1.png', '/images/2.png', '/images/3.png', '/images/4.png', '/images/5.png'],
   },
   { 
     sign: 'Piscis', 
     start: '02-19', 
     end: '03-20',
-    images: ['/images/5.png', '/images/6.png', '/images/7.png', '/images/8.png']
+    images: ['/images/6.png', '/images/7.png', '/images/8.png', '/images/9.png', '/images/10.png']
   },
   { 
     sign: 'Aries', 
     start: '03-21', 
     end: '04-19',
-    images: ['/images/9.png', '/images/10.png', '/images/11.png', '/images/12.png']
+    images: ['/images/11.png', '/images/12.png', '/images/13.png', '/images/14.png', '/images/15.png']
   },
   { 
     sign: 'Tauro', 
     start: '04-20', 
     end: '05-20',
-    images: ['/images/13.png', '/images/14.png', '/images/15.png', '/images/16.png']
+    images: ['/images/16.png', '/images/17.png', '/images/18.png', '/images/19.png', '/images/20.png']
   },
   { 
     sign: 'Géminis', 
     start: '05-21', 
     end: '06-20',
-    images: ['/images/17.png', '/images/18.png', '/images/19.png', '/images/20.png']
+    images: ['/images/21.png', '/images/22.png', '/images/23.png', '/images/24.png', '/images/25.png']
   },
   { 
     sign: 'Cáncer', 
     start: '06-21', 
     end: '07-22',
-    images: ['/images/21.png', '/images/22.png', '/images/23.png', '/images/24.png']
+    images: ['/images/26.png', '/images/27.png', '/images/28.png', '/images/29.png', '/images/30.png']
   },
   { 
     sign: 'Leo', 
     start: '07-23', 
     end: '08-22',
-    images: ['/images/25.png', '/images/26.png', '/images/27.png', '/images/28.png']
+    images: ['/images/31.png', '/images/32.png', '/images/33.png', '/images/34.png', '/images/35.png']
   },
   { 
     sign: 'Virgo', 
     start: '08-23', 
     end: '09-22',
-    images: ['/images/29.png', '/images/30.png', '/images/31.png', '/images/32.png']
+    images: ['/images/36.png', '/images/37.png', '/images/38.png', '/images/39.png', '/images/40.png']
   },
   { 
     sign: 'Libra', 
     start: '09-23', 
     end: '10-22',
-    images: ['/images/33.png', '/images/34.png', '/images/35.png', '/images/36.png']
+    images: ['/images/41.png', '/images/42.png', '/images/43.png', '/images/44.png', '/images/45.png']
   },
   { 
     sign: 'Escorpio', 
     start: '10-23', 
     end: '11-21',
-    images: ['/images/37.png', '/images/38.png', '/images/39.png', '/images/40.png']
+    images: ['/images/46.png', '/images/47.png', '/images/48.png', '/images/49.png', '/images/50.png']
   },
   { 
     sign: 'Sagitario', 
     start: '11-22', 
     end: '12-21',
-    images: ['/images/41.png', '/images/42.png', '/images/43.png', '/images/44.png']
+    images: ['/images/51.png', '/images/52.png', '/images/53.png', '/images/54.png', '/images/55.png']
   },
   { 
     sign: 'Capricornio', 
     start: '12-22', 
     end: '01-19',
-    images: ['/images/45.png', '/images/46.png', '/images/47.png', '/images/48.png']
+    images: ['/images/56.png', '/images/57.png', '/images/58.png', '/images/59.png', '/images/60.png']
   }
 ];
 
 
+
 const songs = {
-  Acuario: 'Canción para Acuario',
-  Piscis: 'Canción para Piscis',
-  Aries: 'Canción para Aries',
-  Tauro: 'Canción para Tauro',
-  Géminis: 'Canción para Géminis',
-  Cáncer: 'Canción para Cáncer',
-  Leo: 'Canción para Leo',
-  Virgo: 'Canción para Virgo',
-  Libra: 'Canción para Libra',
-  Escorpio: 'Canción para Escorpio',
-  Sagitario: 'Canción para Sagitario',
-  Capricornio: 'Canción para Capricornio'
+  Acuario: 'Acuario, ponte pa las situaciones excesivas y la indulgencia',
+  Piscis: 'Piscis, no lo eches todo por la borda, por un amor que no lo merece',
+  Aries: 'Aries, sigue con tu táctica de no darlo todo en bandeja',
+  Tauro: 'Tauro, hoy es un día propicio para que surja un flechazo',
+  Géminis: 'Géminis, coquetea un poco',
+  Cáncer: 'Cáncer, ábrete al amor',
+  Leo: 'Leo, procura seguir a tu voz interior hacia el perreo',
+  Virgo: 'Virgo, día propicio para enamorarte sin prudencia',
+  Libra: 'Libra, haz caso de la intuición',
+  Escorpio: 'Escorpio, deja que todo fluya',
+  Sagitario: 'Sagitario, escucha a la persona que ocupa tu corazón',
+  Capricornio: 'Capricornio, céntrate en el presente y disfrútalo bailando'
 };
 
 const BirthdayForm = ({ setZodiacInfo }) => {
